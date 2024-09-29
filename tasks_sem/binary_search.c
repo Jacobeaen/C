124bf9f9db1d18d17696a78555545ff3e38cd265
Alex <alexander.pushilov@gmail.com>
2024-09-29 22:59:48 +0300

    End of binary_search

--- tasks_sem/binary_search.c
+++ tasks_sem/binary_search.c
@@ -26,10 +26,34 @@ int binary_search(int arr[], int el, int size)
 
 
 int main(void){
-    int arr[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int amount, quantity;
 
-    int index = binary_search(arr, 10, 10);
-    printf("index: %d", index);
+    printf("Put amount of elements in array:\n>> ");
+    scanf("%d", &amount);    
+    quantity = amount;
+
+    printf("Put elements.\n");
+
+    int arr[amount];
+    int used[amount];
+    int j = 0;
+    while (scanf("%d", &arr[j++]) == 1 && --amount > 0){
+    }   
+
+    printf("Your array: [");
+    for (int k = 0; k < quantity; k++){
+        printf("%d", arr[k]);
+        if (k != quantity - 1)
+            printf(", ");
+    }
+    printf("]\n");
+
+    int el;
+    printf("What's element we're looking for:\n>> ");
+    scanf("%d", &el);
+
+    int index = binary_search(arr, el, quantity);
+    printf("Your element: %d at index: %d", el, index);
 
     return 0;
 }
