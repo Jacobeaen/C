98738ed5a1f310c50e030c7125eb34c5f049f847
Alex <alexander.pushilov@gmail.com>
2024-10-01 22:39:37 +0300

    Lab7

--- labs_kir/lab_7.c
+++ labs_kir/lab_7.c
@@ -1,55 +1,89 @@
 #include <stdio.h>
 
-int right(int arr[][5], int i, int j, int current)
+int min2(int x, int y)
 {
-    if (arr[i - 1][j + 1] > current){
-        return 1 + right(arr, i - 1, j + 1, arr[i - 1][j + 1]);       
+    return (x < y) ? x : y;
+}
+
+int min3(int x, int y, int z)
+{
+    return min2(min2(x, y), z);
+}
+
+
+int bottom(int arr[][13], int last, int size_i, int i, int j)
+{
+
+    int current = arr[i][j];
+    if (i < size_i - 1){
+    
+        if (current >= last)
+            return 1 + bottom(arr, arr[i][j], size_i, i + 1, j);
+        return 0;
     }
+    else if (i == size_i - 1)
+        return current >= last;
     return 0;
 }
 
-int left(int arr[][5], int i, int j, int current)
+int left(int arr[][13], int last, int i, int j)
 {
-    if (arr[i - 1][j - 1] > current){
-        return 1 + right(arr, i - 1, j - 1, arr[i - 1][j - 1]);       
+    int current = arr[i][j];
+    if (i > 0 && j > 0){
+        if (current >= last)
+            return 1 + left(arr, arr[i][j], i - 1, j - 1);
+        return 0;
     }
+    if (i == 0 || j == 0)
+        return current >= last;
     return 0;
+
 }
 
-int bottom(int arr[][5], int i, int j, int current)
+int right(int arr[][13], int last, int size_j, int i, int j)
 {
-    if (arr[i + 1][j] > current){
-        return 1 + right(arr, i + 1, j, arr[i + 1][j]);       
+    int current = arr[i][j];
+
+    if (i > 0 && j < size_j - 1){
+        if (current >= last)
+            return 1 + right(arr, arr[i][j], size_j, i - 1, j + 1);
+        return 0;
     }
+    else if (i == 0 || j == size_j - 1)
+        return current >= last;
+    
     return 0;
+
 }
 
 
 int main(void)
 {
-    int arr[5][5];
+    int arr[11][13];
+    int size_i = 11, size_j = 13;
 
-    for (int i = 0; i < 5; i++){
-        for (int j = 0; j < 5; j++){
+    for (int i = 0; i < 11; i++){
+        for (int j = 0; j < 13; j++){
             scanf("%d", &arr[i][j]);
         }
     }
 
-    int y, count = 1;
-    // Границы для i и j
-    for (int i = 1; i < 4; i++){
-        for (int j = 0; j < 5; j++){
-            int current = arr[i][j];
-            for (int k = 0; k < i; k++)    
-                y = right(arr, i, j, current) + left(arr, i, j, current) + bottom(arr, i, j, current);
-        
-            count = (y > count) ? y : count;
+    printf("\n");
+    int count = 1;
+    for (int i = 1; i < 10; i++){
+        for (int j = 1; j < 12; j++){
+            int left_y = left(arr, arr[i][j], i - 1, j - 1);
+            int bottom_y = bottom(arr, arr[i][j], size_i, i + 1, j);
+            int right_y = right(arr, arr[i][j], size_j, i - 1, j + 1);
+            
+            int y = min3(left_y, bottom_y, right_y);
+            printf("i: %d, j: %d, lef: %d, bot: %d, rig: %d, y: %d\n", i, j, left_y, bottom_y, right_y, y);
+
+            count = (y >= count) ? (y + 1) : count;
         }
-    } 
+        printf("\n");
+    }
 
-    printf("%d", y);
-
-
-
+    printf("%d", count);
     return 0;
 }
