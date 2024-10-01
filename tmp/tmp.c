aecffb189f75ca11fb3cf8d848cd90bf819b1b94
Alex <alexander.pushilov@gmail.com>
2024-10-01 17:18:41 +0300

    delete duplicate

--- tmp/tmp.c
+++ tmp/tmp.c
@@ -1,7 +1,28 @@
 #include <stdio.h>
 
+int next(int arr[], int size, int last, int i)
+{
+    int current = arr[i];
+
+    if (i < size - 1){
+        if (arr[i] > last)
+            return 1 + next(arr, size, arr[i], i + 1);
+        return 0;
+    }
+    return arr[i] > last;
+    
+
+
+}
+
 int main(void)
 {
+    int arr[5] = {1, 3, 3, 0, 7};
+
+    for (int i = 0; i < 4; i++){
+        int c = next(arr, 5, arr[i], i + 1);
+        printf("%d ", c);
+    }
 
     return 0;
 }
