65a95aeb18927308dd78be96292edb62badd3bc6
Alex <alexander.pushilov@gmail.com>
2024-10-04 02:01:28 +0300

    Largest subsequence

---
+++ labs_kir/lab_2_1.c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+
+/*
+Условие: На вход дается число n - кол-во элементов массива
+Определите максимальную строго возрастающую подпоследовательность элементов этого массива.
+*/
+
+int main(void)
+{
+
+    int amount, count = 1;
+    scanf("%d", &amount);
+
+    int number, max_len = 0;
+    int last_num = -1;
+    int flag = 1;
+    for (int i = 0; i < amount; i++){
+        scanf("%d", &number);
+        
+        if (i == 0){
+            last_num = number;
+            continue;
+        }
+
+        if (number > last_num){
+            count++;
+            flag = 1;
+        } else {
+            max_len = (max_len < count) ? count : max_len;
+            count = 1;
+            flag = 0;
+        }
+
+        last_num = number;
+    }
+
+    if (flag && max_len < count)
+        max_len = count;
+
+    printf("%d", max_len);
+
+    return 0;
+}
