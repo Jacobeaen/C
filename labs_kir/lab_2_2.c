4cf888f981a911f4ff645b144e079c61e358130f
Alex <alexander.pushilov@gmail.com>
2024-10-04 13:47:02 +0300

    Do int plus

--- labs_kir/lab_2_2.c
+++ labs_kir/lab_2_2.c
@@ -64,7 +64,7 @@ int main(void)
                         number2_int[int2_i++] = string[i] - 48;
                     }
                     else if (string[i] != '0' && !num2_zero_int){
-                        num1_zero_int = 1;
+                        num2_zero_int = 1;
                         number2_int[int2_i++] = string[i] - 48;
                     }
                 }
@@ -76,45 +76,70 @@ int main(void)
         }
         else{
             if (num1_zero_int){
-                number1_int[int1_i] = string[i] - 48;
+                number1_int[int1_i++] = string[i] - 48;
             }
             else if (string[i] != '0' && !num1_zero_int){
                 num1_zero_int = 1;
-                number1_int[int1_i] = string[i] - 48;
+                number1_int[int1_i++] = string[i] - 48;
             }
         }
 
     i++;
     }
 
+
+
+
     int max_float = max(float1_i, float2_i);
     int overflow_float = 0, num3_fl_dig;
     for (int index = max_float - 1; index >= 0; index--){
-        printf("%d %d %d\n", number1_float[index], number2_float[index], overflow_float);
         num3_fl_dig = (number1_float[index] + number2_float[index] + overflow_float) % 10;
         overflow_float = (number1_float[index] + number2_float[index] + overflow_float) / 10; 
 
         number3_float[index] = num3_fl_dig;   
     }
 
+
+
+
+
+    int extra_1 = 0, extra_2 = 0;
+    int flag1 = 0, flag2 = 0;
+
+    printf("int1: %d int2: %d\n\n", int1_i, int2_i);
+
     int max_int = max(int1_i, int2_i);
-    int  overflow_int = 0, num3_in_dig;
-    for (int index = max_int - 1; index >= 0; index--){
-        int flag = 0;
+    if (max_int == int1_i){
+        extra_2 = int1_i - int2_i;
+        flag2 = 1;
+    }
+    else {
+        extra_1 = int2_i - int1_i;
+        flag1 = 1; 
+    }
+
+    int num3_in_dig, overflow_int = 0, sum_dig = 0, overflow_float_to_int = 0;
+    printf("max int: %d\n", max_int);
+    for (int index = max_int - 1; index >= 0; index--, overflow_float_to_int = 0){
         if (index == max_int - 1){
             if (overflow_float)
-                flag = 1;
+                overflow_float_to_int++;
         }
 
-        num3_in_dig = (number1_int[index] + number2_int[index] + overflow_int + flag) % 10;
-        overflow_float = (number1_int[index] + number2_int[index] + overflow_int + flag) / 10;
+
+        sum_dig = number1_int[index - extra_1 + flag1] + number2_int[index - extra_2 + flag2] + overflow_int + overflow_float_to_int;
+        
+        num3_in_dig = sum_dig % 10;
+        overflow_int = sum_dig / 10;
+        printf("1:%d 2:%d o:%d i: %d\n", number1_int[index - extra_1 + flag1], number2_int[index - extra_2 + flag2], overflow_int, index);
+        printf("dig: %d sum: %d, over: %d\n\n", num3_in_dig, sum_dig, overflow_int);
 
         number3_int[index] = num3_in_dig;
     }
 
-    for (int i = 0; i < max_int; i++)
-        printf("%d", number3_int[i]);
-    printf(".");
+    for (int i = 0; i < max_int; i++){}
+        //printf("%d", number3_int[i]);
+    //printf(".");
 
 
     int zero_index = 0, first_dig = 0;
@@ -126,7 +151,7 @@ int main(void)
 
 
     for (int k = 0; k <= zero_index; k++)
-        printf("%d", number3_float[k]);
+        //printf("%d", number3_float[k]);
     return 0;
 
 }
