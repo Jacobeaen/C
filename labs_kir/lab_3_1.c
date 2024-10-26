f7a89695a856237b6b960710dbf5326d80419555
Alex <alexander.pushilov@gmail.com>
2024-10-26 15:46:13 +0300

    Lab 3_1

--- labs_kir/lab_3_1.c
+++ labs_kir/lab_3_1.c
@@ -1,10 +1,22 @@
 #include <stdio.h>
 
+/*
+Условие: на вход подается строка
+Нужно заменить цепочку подряд идущих символов на один символ
+aassbbbttteeeeod22222f44f
+*/
+
+
 int main(void){
 
-    char string[101];
-    char new_string[101];
-    fgets(string, 100, stdin);
+    char string[10001];
+    char new_string[10001];
+    fgets(string, 10000, stdin);
+
+    if (string[0] == '\n'){
+        printf("[\n]");
+        return 0;
+    }
 
     int i = 0, j = 0;
     while (string[i] != '\0' && string[i] != '\n'){
@@ -18,6 +30,7 @@ int main(void){
 
     new_string[j] = '\0';
 
+    
     printf("%s", new_string);
 
 
