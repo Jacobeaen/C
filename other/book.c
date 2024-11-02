e73108f98eefb54499650d266000b7babab7ee21
Alex <alexander.pushilov@gmail.com>
2024-11-02 21:23:23 +0300

    rm tmp.c

--- other/book.c
+++ other/book.c
@@ -10,6 +10,22 @@ struct Book {
     double price;
 };
 
+struct Book* expandArray(struct Book *array, int *capacity, bool *success)
+{
+    int new_capacity = *capacity * 2;
+    struct Book *new_array = realloc(array, sizeof(struct Book) * new_capacity);
+
+    *success = true;
+    if (new_array == NULL){
+        *success = false;
+        return array;
+    }
+    *capacity = new_capacity;
+
+    return new_array;
+}
+
+
 void setBookName(char *name)
 {
     fgets(name, 29, stdin);
@@ -31,8 +47,6 @@ void setBook(char *name,
 
     printf("Price: ");
     scanf("%lf", price);
-
-    printf("\n");
 }
 
 void showMenu(char *menu[], int size)
@@ -60,7 +74,7 @@ void addBook(struct Book array[],
 
         // printf("name: %s, book_name: %s", name, book.name);
         if (strcmp(book.name, name) == 0){
-            printf("Sorry, this book is already in your library!\n\n");
+            printf("Sorry, this book is already in your library!\n");
             *in_library = true;
             return ;
         }
@@ -82,7 +96,7 @@ void addBook(struct Book array[],
 void showBooks(struct Book *array, int size)
 {
     if (!size){
-        printf("Your library is empty\n\n");
+        printf("Your library is empty\n");
         return ;
     }
 
@@ -94,26 +108,53 @@ void showBooks(struct Book *array, int size)
     }
 }
 
-void findBook(struct Book *array, int size, char *book_name)
+int findBook(struct Book *array, int size, char *book_name, bool get_index)
 {
-    printf("Your library:\n");
+    
+    if (!get_index)
+        printf("Result:\n");
+
     for (int i = 0; i < size; ++i){
         struct Book book = array[i];
 
-        if (!strcmp(book.name, book_name)){
+        if (strcmp(book.name, book_name) == 0){
+            if (get_index)
+                return i;
+
             printf("Book name: %s", book_name);
             printf("Book author: %s", book.author);
             printf("Book year: %d\n", book.year);
-            printf("Book price: %lf\n", book.price);
+            printf("Book price: %.2lf\n", book.price);
 
-            break;
+            return 0;
         }
     }
+
+    return -1;
+
 }
 
-void deleteBook(struct Book *array)
+struct Book* deleteBook(struct Book *array, char *name, int size, int del_index)
 {
+    struct Book *new_array = malloc(sizeof(struct Book) * (size - 1));
 
+    if (new_array == NULL){
+        printf("Sorry, can't delete book, try again");
+        return array;
+    }
+
+    for (int i = 0; i < size; ++i){
+        if (i == del_index){
+            continue;        
+        }
+
+        new_array[i] = array[i];
+    }
+
+    free(array);
+    array = new_array;
+
+    return array;
 }
 
 int main()
@@ -127,17 +168,21 @@ int main()
         "5. Exit"
     };
 
-    struct Book *array = malloc(sizeof(struct Book) * 5);
+    int capacity = 1;
+    struct Book *array = malloc(sizeof(struct Book) * capacity);
     
     char choice = '_';
+
     char name[30];
     char author[30];
     int year;
     double price;
 
-    int i = 0;
+    int size = 0;
     char tmp = '1';
-    
+
+    bool success;
+    bool is_deleted;
 
     showMenu(menu, 6);
     printf(">> ");
@@ -151,31 +196,75 @@ int main()
                 setBook(name, author, &year, &price);
                 scanf("%c", &tmp);
 
-                addBook(array, i, name, author, year, price, &in_library);
+                if (size >= capacity){
+                    array = expandArray(array, &capacity, &success);
+                    if (!success){
+                        printf("Sorry, we can't dedicate more space for your books\n");
+                        printf("Try again\n");
+                        break;
+                    }
+                }
+
+                addBook(array, size, name, author, year, price, &in_library);
                 if (!in_library)
-                    i++;
+                    size++;
 
                 break;
+
             case '2':
-                showBooks(array, i);
+
+                printf("\n");
+                showBooks(array, size);
                 break;
+
             case '3':
+
                 char book_name[30];
-                if (i == 0){
-                    printf("Your library is empty\n\n");
+                if (size == 0){
+                    printf("\n");
+                    printf("Your library is empty\n");
                     break;
                 }
 
                 setBookName(book_name);
-                findBook(array, i, book_name);
+                printf("\n");
+                int answer = findBook(array, size, book_name, false);
+                
+                if (answer == -1)
+                    printf("Book isn't in your library\n");
+
                 break;
+
             case '4':
-                printf("This func will be added soon!\n");
+
+                setBookName(book_name);
+
+                int del_index = findBook(array, size, book_name, true);
+                if (del_index == -1){
+                    printf("This book isn't in your library\n");
+                    break;
+                }
+                
+                char sure[3];
+                printf("Are you sure (y/n)?\n>> ");
+                fgets(sure, sizeof(sure), stdin);
+
+                if (strncmp(sure, "n", 1) == 0){
+                    printf("Exit...\n");
+                    break;
+                }
+
+                array = deleteBook(array, book_name, size, del_index);
+                printf("Book deleted from your library\n");
+                size--;
+
                 break;
+
             default:
-                printf("Sorry, incorrect choice!\nWrite number again.\n\n");
+                printf("Sorry, incorrect choice!\nWrite number again.\n");
         }
 
+        printf("\n");
         showMenu(menu, 6);
         printf(">> ");
     }
