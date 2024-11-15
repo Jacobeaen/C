02d0f61822f0b3c083dfc29ff4c80b2026d80e7e
Alex <alexander.pushilov@gmail.com>
2024-11-15 23:28:54 +0300

    Simple plus

--- labs_kir/lab_2_2.c
+++ labs_kir/lab_2_2.c
@@ -17,170 +17,169 @@ _ - цифра, + - знак сложения. Вывести результат
 вывод: 910.0999
 */
 
+void FillIntArray(int array[], char *string, int *position, int *counter, char stop)
+{
+    int is_zero = 1;
+    for (int i = 0; string[*position] != stop; (*position)++){
+        if (is_zero){
+            if (string[*position] != '0'){
+                is_zero = 0;
+                array[i++] = string[*position] - '0';
+                (*counter)++;
+            }
+        }
+        else {
+            array[i++] = string[*position] - '0';
+            (*counter)++;
+        }
+    }
+
+    (*position)++;
+}
+
+void FillFloatArray(int array[], char *string, int *position, int *counter, char stop)
+{
+    for (int i = 0; string[*position] != stop && string[*position] != '\0'; (*position)++){
+        array[i++] = string[*position] - '0';
+        (*counter)++;
+    }
+
+    for (int i = (*counter) - 1; i >= 0; i--){
+        if (array[i] != 0){
+            *counter = i + 1;
+            break;
+        }
+    }
+
+   (*position)++;
+}
+
+void ShiftIntArray(int array[], int shift, int size)
+{
+    for (int i = size - 1; i >= 0; i--){
+        array[i + shift] = array[i];
+        array[i] = 0;
+    }
+}
+
+int ShiftLength(int x, int y){
+    int result = x - y;
+    
+    return (result >= 0) ? result : -result;
+}
+
 int max(int x, int y){
     return (x > y) ? x : y;
 }
 
+
 int main(void)
 {
     char string[100];
-    
+
     int number1_int[100] = {0}, number1_float[100] = {0};
     int number2_int[100] = {0}, number2_float[100] = {0};
     int number3_int[100] = {0}, number3_float[100] = {0};
 
-    fgets(string, 51, stdin);
+    fgets(string, 100, stdin);
 
-    int plus = 0;
-    int dot_1 = 0, dot_2 = 0;
-
-    int num1_zero_int = 0;
-    int num2_zero_int = 0;
-
-    int int1_i = 0, float1_i = 0;
-    int float2_i = 0;
-    int int2_i = 0;
     int i = 0;
-    while (string[i] != '\n' && string[i] != '\0'){
-        if (string[i] == '.' && !dot_1){
-            dot_1 = i++;
-            continue;
-        }
+    int int_1 = 0, int_2 = 0;
+    int float_1 = 0, float_2 = 0;
 
-        if (string[i] == '+'){
-            plus = i++;
-            continue;
-        }
+    FillIntArray(number1_int, string, &i, &int_1, '.');
+    FillFloatArray(number1_float, string, &i, &float_1, '+');
+    FillIntArray(number2_int, string, &i, &int_2, '.');
+    FillFloatArray(number2_float, string, &i, &float_2, '\n');
 
-       if (string[i] == '.' && dot_1){
-            dot_2 = i++;
-            continue;
-        }
 
-        if (dot_1){
-            if (plus){
-                if (dot_2)
-                    number2_float[float2_i++] = (string[i] - 48);
-                else{
-                    if (num2_zero_int)
-                        number2_int[int2_i++] = (string[i] - 48);
-                    else if (string[i] != '0' && !num2_zero_int){
-                        num2_zero_int = 1;
-                        number2_int[int2_i++] = (string[i] - 48);
-                    }
-                }
-            }
-            else            
-                number1_float[float1_i++] = (string[i] - 48);
+    // Сдвигаем меньший из массивов, для поразрядного сложения цифр
+    int shift = ShiftLength(int_1, int_2);
+    if (shift != 0){
+        if (int_1 > int_2)
+            ShiftIntArray(number2_int, shift, int_2);
+        else
+            ShiftIntArray(number1_int, shift, int_1);
+    }
 
-        
+    // Дробная часть
+    int longest_float = max(float_1, float_2);
+    int stop_f = longest_float;
+    int overflow_f = 0;
+    int only_zeros_f = 1;
+
+
+    // Поразрядное сложение, only_zeros_f - первый незначащий ноль
+    for (int i = longest_float - 1; i >= 0; i--){
+
+        int sum_dig = number1_float[i] + number2_float[i] + overflow_f;
+        overflow_f = sum_dig / 10;
+
+        int digit = sum_dig % 10;
+        if (digit == 0 && only_zeros_f){
+            stop_f = i;
         }
         else{
-            if (num1_zero_int){
-                number1_int[int1_i++] = (string[i] - 48);
-            }
-            else if (string[i] != '0' && !num1_zero_int){
-                num1_zero_int = 1;
-                number1_int[int1_i++] = (string[i] - 48);
-            }
-        }
-    i++;
-    }
-
-    int max_float = max(float1_i, float2_i);
-    int overflow_float = 0, num3_fl_dig = 0;
-    int sum_dig = 0;
-
-    // printf("%d\n", max_float);
-    for (int index = max_float - 1; index >= 0; index--){
-
-        // printf("%d - %d - %d\n", number1_float[index], number2_float[index], overflow_float);
-        sum_dig = number1_float[index] + number2_float[index] + overflow_float;
-        num3_fl_dig = sum_dig % 10;
-        overflow_float = sum_dig / 10; 
-
-        number3_float[index] = num3_fl_dig;   
-        // printf("%d\n", num3_fl_dig);
-        // printf("%d\n", number3_float[index]);
-        
-    }
-
-    int extra_1 = 0, extra_2 = 0;
-    int flag1 = 0, flag2 = 0;
-
-
-    int max_int = max(int1_i, int2_i);
-    
-    if (int1_i == int2_i)
-        {}
-    else if (max_int == int1_i){
-        extra_2 = int1_i - int2_i;
-        flag2 = 1;
-    }
-    else {
-        extra_1 = int2_i - int1_i;
-        flag1 = 1; 
-    }
-
-    int num3_in_dig = 0, overflow_int = 0, overflow_float_to_int = 0;
-    int end = 49;
-    for (int index = max_int - 1; index >= 0; index--, overflow_float_to_int = 0){
-        if (index == max_int - 1){
-            if (overflow_float)
-                overflow_float_to_int++;
+            only_zeros_f = 0;
         }
 
-        if (index < extra_1) extra_1 = 0;
-        if (index < extra_2) extra_2 = 0;
+        number3_float[i] = digit; 
+    }
 
-        sum_dig = number1_int[index - extra_1] + number2_int[index - extra_2] + overflow_int + overflow_float_to_int;
+    // Целая часть
+    int longest_int = max(int_1, int_2);
+    int overflow_i = 0;
+    int only_zeros_i = 1;
+
+    // only_zeros_i - Если все нули в целой части
+    // Оставляем 0-ой элемент массива под переполнение
+    for (int i = longest_int - 1; i >= 0; i--, overflow_f = 0){
+
+        int sum_dig = number1_int[i] + number2_int[i] + overflow_f + overflow_i;
+        overflow_i = sum_dig / 10;
+
+        int digit = sum_dig % 10;
         
-        if (extra_1) number1_int[index - extra_1] = 0;
-        if (extra_2) number2_int[index - extra_2] = 0;
+        if (digit != 0){
+            only_zeros_i = 0;
+        }
 
-        num3_in_dig = sum_dig % 10;
-        overflow_int = sum_dig / 10;
-
-        number3_int[end--] = num3_in_dig;
-    }
-    if (overflow_int){
-        number3_int[end] = overflow_int;
-        end--;
+        number3_int[i + 1] = digit; 
     }
 
+    // Если переполнение - заполняем 0-ой элемент массива
+    int start = 1;
+    if (overflow_i > 0){
+        start = 0;
+        number3_int[0] = overflow_i;
+        only_zeros_i = 0;
+    }
 
-    if (number3_int[end + 1] == 0 && number3_float[0] == 0){
-        printf("%d", 0);
+    // Если нули целая и дробная части - 0
+    if (only_zeros_i && only_zeros_f){
+        printf("0");
         return 0;
     }
 
+    // Если нули только в целой части (цикл не выполнится)
+    if (only_zeros_i && !only_zeros_f)
+        printf("0");
 
-    int zero = 1;
-    for (int p = end + 1; p < 50; p++){
-        printf("%d", number3_int[p]);
-        zero = 0;
-    } 
-    
+    // Вывод целой части
+    for (int i = start; i < longest_int + 1; i++){
+        printf("%d", number3_int[i]);
+    }
 
-
-    int zero_index = 0, first_dig = 0;
-    for (int j = max_float - 1; j >= 0; j--)
-        if (number3_float[j] != 0 && !first_dig){
-            zero_index = j;
-            break;
-        }
-
-    if (zero_index == 0 && number3_float[zero_index] == 0)
+    // Если в дробной части одни нули
+    if (only_zeros_f)
         return 0;
-    
-    if (zero)
-        printf("%d", 0);
     printf(".");
     
-    for (int k = 0; k <= zero_index; k++)
-        printf("%d", number3_float[k]);
 
-
+    // Вывод дробной части
+    for (int i = 0; i < stop_f; i++){
+        printf("%d", number3_float[i]);
+    }
+    
     return 0;
-
 }
