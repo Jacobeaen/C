3add2f17b07a67d27aa83b893948c265c8320b55
Alex <alexander.pushilov@gmail.com>
2024-11-24 15:14:22 +0300

    choice_funcs

--- labs_god/lab4/choice_sort/choice_funcs.h
+++ labs_god/lab4/choice_sort/choice_funcs.h
@@ -1,7 +1,7 @@
 #ifndef CHOICE_FUNCS_H
 #define CHOICE_FUNCS_H
 
-// Прототипы. Реализации лежат в choice_funcs.c
+// Прототипы. Реализации лежат в сhoice_funcs.c
 int findMin(int array[], int size, int start, int *compares);
 void Swap(int array[], int i, int j);
 int* choiceSort(int array[], int size, int *compares, int *swaps);
