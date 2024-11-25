05d2b0eac88cb07c10ccb7ad0104033bfb0ffe8e
Alex <alexander.pushilov@gmail.com>
2024-11-25 22:42:09 +0300

    merge_funcs

--- labs_god/lab4/merge_sort/merge_funcs.h
+++ labs_god/lab4/merge_sort/merge_funcs.h
@@ -3,7 +3,7 @@
 
 // Прототипы. Реализации находятся в merge_funcs.c
 void copyNumbers(int[], int[], int, int);
-int *mergeArrays(int array1[], int size1, int array2[], int size2, int *compares, int *swaps);
-int *mergeSort(int[], int, int *compares, int *swaps);
+int *mergeArrays(int array1[], int size1, int array2[], int size2, long long *compares, long long *swaps);
+int *mergeSort(int[], int, long long *compares, long long *swaps);
 
 #endif
